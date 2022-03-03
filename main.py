1d4fb3cd8a80f5d0478f51a934f0a5d63242f34d
Nam Tran <namtran211101@gmail.com>
2022-03-03 20:42:11 +1100

    Redo Layout for the overview tab

--- main.py
+++ main.py
@@ -1,83 +1,50 @@
+# 
 import sys
-from PySide6 import QtWidgets
-import PySide6
+import os
+from PySide6.QtCore import *
+from PySide6.QtGui import *
+from PySide6.QtWidgets import *
+from PySide6.QtCharts import *
 
-from MainWindow import Ui_MainWindow
-from SerialHandler import Serial_Emulator
+from ui import MainDash, dashboard
 
-import time
+class Color(QWidget):
 
-from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
-    QMetaObject, QObject, QPoint, QRect,
-    QSize, QTime, QUrl, Qt, QThread, Signal)
+    def __init__(self, color):
+        super(Color, self).__init__()
+        self.setAutoFillBackground(True)
 
-Serial = Serial_Emulator()
+        palette = self.palette()
+        palette.setColor(QPalette.Window, QColor(color))
+        self.setPalette(palette)
 
-class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
-    #Text colours for different widgets
-    RedTextColour = PySide6.QtGui.QColor("red")
-    GreenTextColour = PySide6.QtGui.QColor("green")
 
+class MainWindow(QMainWindow):
     def __init__(self):
-        super(MainWindow, self).__init__()
-        self.setupUi(self)
-        self.ComPortSelect.addItems(Serial.get_available_port())
-        self.ComBaudSelect.addItems(["57600", "115200"])
-        self.ConnectButton.clicked.connect(self.ConnectClicked)
-        self.ComSerialClearButton.clicked.connect(self.ClearClicked)
+        QMainWindow.__init__(self)
 
-    #This function handles when the Connect button is clicked
-    def ConnectClicked(self):
-        if(self.ConnectButton.isChecked()):
-            #Try to open serial port if there was a problem uncheck the button and tell user of error
-            try:
-                Serial.OpenPort(self.ComPortSelect.currentText(), self.ComBaudSelect.currentText())
-                self.ConnectButton.setText("Disconnect")
-                self.ComStatusOutput.setTextColor(self.GreenTextColour)
-                self.ComStatusOutput.append("SUCCESS: CONNECTED TO {} AT {}".format(self.ComPortSelect.currentText(), self.ComBaudSelect.currentText()))
+        self.resize(1000,700)
+        self.setWindowTitle("Live Telemtry")
 
-                #Create worker thread that continuously reads from the COM PORT in the background
-                self.SerialReaderThread = WorkerThread()
-                self.SerialReaderThread.start()
-                self.SerialReaderThread.SerialLine.connect(self.UpdateSerialMonitor) #Run UpdateSerialMonitor everytime an emit is received from SerialReaderThread
+        self.container = QFrame()
+        #self.container.setObjectName("container")
+        #self.container.setStyleSheet("#container { background-color : #222}")
+        
+        self.tabs = QTabWidget()
+        self.tabs.setTabPosition(QTabWidget.North)
+        self.tabs.setMovable(True)
 
-            except:
-                self.ComStatusOutput.setTextColor(self.RedTextColour)
-                self.ComStatusOutput.append("ERROR: FAILED TO CONNECT TO COM PORT")
-                self.ConnectButton.toggle()
-        else:
-            self.SerialReaderThread.terminate() #Terminate the Serial Reader thread
-            self.ConnectButton.setText("Connect")
-            Serial.ClosePort()
-            self.ComStatusOutput.setTextColor(self.GreenTextColour)
-            self.ComStatusOutput.append("SUCCESS: DISCONNECTED FROM {}".format(self.ComPortSelect.currentText()))  
+        for n, color in enumerate(["red", "green", "blue", "yellow"]):
+            self.tabs.addTab(Color(color), color)
+        dashboard = MainDash()
+        self.tabs.addTab(dashboard,"Dashboard")
 
-    #This function clears the COM viewer when the clear COM viewer button is clicked
-    def ClearClicked(self):
-        self.ComSerialOutput.setText("")
+        #self.container.setLayout(self.layout)
+        self.setCentralWidget(self.tabs)
 
-    #This function updates the Serial monitor with the serial data read
-    def UpdateSerialMonitor(self, SerialLineRead):
-        self.ComSerialOutput.append(SerialLineRead)
+        self.show()
 
-#Thread that runs in the background when serial port is open and reads the COM port
-class WorkerThread(QThread):
-    SerialLine = Signal(str)
-    def run(self):
-        while True:
-            LineRead = Serial.Read()
-
-            #If line read was empty then there was no data
-            if(not(LineRead == '')):
-                #Transmits data to the MainWindow class
-                self.SerialLine.emit(LineRead)
-
-
-
-app = QtWidgets.QApplication(sys.argv)
-
-window = MainWindow()
-window.show()
-#When window opens
-app.exec()
-#When program closes
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    sys.exit(app.exec())
